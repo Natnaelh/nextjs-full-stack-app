dbf2a55103d47e076eab9156ec0e3a39c23a390f
NatnaelH <natnaelhabtamu0@gmail.com>
2022-11-15 22:53:19 +0300

    fix: import error with sidebarlayout

--- components/layouts/sidebar/SideBarLayout.tsx
+++ components/layouts/sidebar/SideBarLayout.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './SidebarLayout.module.css';
+import styles from './SideBarLayout.module.css';
 
 export interface ISideBarLayout {
   // sampleTextProp: string;
