77caaafa62144e631a4840e8b04bd82c04c23755
NatnaelH <natnaelhabtamu0@gmail.com>
2022-11-16 00:59:05 +0300

    feat: implement tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { NextPageWithLayout } from './page';
 const Home: NextPageWithLayout = () => {
   return (
     <section className={styles.main}>
-      <h1 className={styles.title}>
+      <h1 className="text-8xl  ">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
       <CatCard {...mockCatCardProps.base} />
