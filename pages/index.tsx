58e07f13306c44de245b4333ada325110fa5bd01
NatnaelH <natnaelhabtamu0@gmail.com>
2022-11-22 02:32:33 +0300

    feat: implement header and footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 // import CatCard from '../components/cards/cat/CatCard';
 // import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
+
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
       <Image
@@ -15,6 +19,12 @@ const Home: NextPageWithLayout = () => {
         priority
       />
       <Search />
+      <p>
+        Google offered in:{' '}
+        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
+          <a className="underline text-blue-600"> Français</a>
+        </Link>
+      </p>
     </section>
   );
 };
