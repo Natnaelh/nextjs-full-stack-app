b3307e7f23db6d61b2936928a5c08921301dd4f7
NatnaelH <natnaelhabtamu0@gmail.com>
2022-11-22 02:01:58 +0300

    feat: implement search component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
+// import CatCard from '../components/cards/cat/CatCard';
+// import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
+import Image from 'next/image';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SideBarLayout';
-import styles from '../styles/Home.module.css';
+import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
-
 const Home: NextPageWithLayout = () => {
   return (
-    <section className={styles.main}>
-      <h1 className="text-8xl  ">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <CatCard {...mockCatCardProps.base} />
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <Image
+        src="/Google-logo.png"
+        alt="Google Logo"
+        width={272}
+        height={92}
+        priority
+      />
+      <Search />
     </section>
   );
 };
@@ -21,7 +24,7 @@ export default Home;
 Home.getLayout = (page) => {
   return (
     <PrimaryLayout>
-      <SidebarLayout />
+      {/* <SidebarLayout /> */}
       {page}
     </PrimaryLayout>
   );
